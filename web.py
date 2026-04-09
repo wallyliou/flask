efcac1b532caffae1bdd8d373535f261a1afb8c8
wallyliou <wally950831@gmail.com>
2026-04-09 11:32:32 +0800

    0409b

--- web.py
+++ web.py
@@ -1,6 +1,25 @@
 from flask import Flask, render_template, request
 
 from datetime import datetime
+
+import os
+import json
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+# 判斷是在 Vercel 還是本地
+if os.path.exists('serviceAccountKey.json'):
+    # 本地環境：讀取檔案
+    cred = credentials.Certificate('serviceAccountKey.json')
+else:
+    # 雲端環境：從環境變數讀取 JSON 字串
+    firebase_config = os.getenv('FIREBASE_CONFIG')
+    cred_dict = json.loads(firebase_config)
+    cred = credentials.Certificate(cred_dict)
+
+firebase_admin.initialize_app(cred)
+
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -12,6 +31,7 @@ def index():
     link += "<a href = /welcome?u=宇崴&d=靜宜資管&c=資訊管理導論>Get傳值</a><hr>"
     link += "<a href = /account>Post傳值</a><hr>"
     link += "<a href = /math>次方與根號計算</a><hr>"
+    link += "<a href=/read>讀取Firestore資料</a><hr>"
 
     return link
 
@@ -71,6 +91,16 @@ def math():
     
     # GET 請求時只顯示初始頁面
     return render_template("math.html")
+
+@app.route("/read")
+def read():
+    Result = ""
+    db = firestore.client()
+    collection_ref = db.collection("靜宜資管")    
+    docs = collection_ref.get()    
+    for doc in docs:         
+        Result += str(doc.to_dict()) + "<br>"    
+    return Result
 
 
 if __name__ == "__main__":
