23c2d99bd257794effac4101e152e7393d548ad7
wallyliou <wally950831@gmail.com>
2026-04-30 11:40:17 +0800

    2026/4/30

--- web.py
+++ web.py
@@ -41,7 +41,46 @@ def index():
     link += "<a href=/read3>讀取Firestore資料(動態輸入關鍵字)</a><hr>"
     link += "<a href=/spider>爬取子青老師本學期課程</a><hr>"
     link += "<a href=/movie1>爬取即將上映電影</a><hr>"
+    link += "<a href=/spidermovie>讀取開眼電影即將上映影片，寫入Firestore</a><hr>"
     return link
+
+@app.route("/spidermovie")
+def spidermovie():
+    Result=""
+    db = firestore.client()
+
+    url = "http://www.atmovies.com.tw/movie/next/"
+    Data = requests.get(url)
+    sp = BeautifulSoup(Data.text, "html.parser")
+    Data.encoding = "utf-8"
+
+    lastupdate=sp.find(class_="smaller09").text.replace("更新時間：","")
+
+    result=sp.select(".filmListAllX li")
+
+    total = 0
+    for item in result:
+        total += 1
+        title = item.find(class_="filmtitle").text
+        picture = "https://www.atmovies.com.tw"+item.find("img").get("src")
+        hyperlink = "https://www.atmovies.com.tw"+item.find("a").get("href")
+        movie_id= item.find("a").get("href").replace("/movie/","").replace("/","")
+        showDate=item.find(class_="runtime").text[5:15]
+
+        doc = {
+            "title": title,
+            "picture": picture,
+            "hyperlink": hyperlink,
+            "showDate": showDate,
+            "lastupdate": lastupdate
+        }
+
+        doc_ref = db.collection("電影2B").document(movie_id)
+        doc_ref.set(doc)
+
+    Result+="網站更新日期"+lastupdate+"<br>"
+    Result+="總共爬取"+str(total)+"部電影到資料庫"+"<br>"
+    return Result
 
 @app.route("/mis")
 def course():
