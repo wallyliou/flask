5ea2ffd4745ab4e4d4f2fcd9d9cc39cb44769b59
wallyliou <wally950831@gmail.com>
2026-05-14 11:41:16 +0800

    5/14_3

--- web.py
+++ web.py
@@ -58,9 +58,21 @@ def webhook():
     action =  req["queryResult"]["action"]
     #msg =  req["queryResult"]["queryText"]
     #info = "我是劉宇崴設計的機器人，動作：" + action + "； 查詢內容：" + msg
+
     if (action == "rateChoice"):
         rate =  req["queryResult"]["parameters"]["rate"]
-        info = "我是劉宇崴設計的機器人，您選擇的電影分級是：" + rate
+        info = "我是劉宇崴設計的機器人，您選擇的電影分級是：" + rate + "，相關電影：\n"
+
+    db = firestore.client()
+        collection_ref = db.collection("電影含分級")
+        docs = collection_ref.get()
+        result = ""
+        for doc in docs:
+            dict = doc.to_dict()
+            if rate in dict["rate"]:
+                result += "片名：" + dict["title"] + "\n"
+                #result += "介紹：" + dict["hyperlink"] + "\n\n"
+        info += result
 
     return make_response(jsonify({"fulfillmentText": info}))
 
